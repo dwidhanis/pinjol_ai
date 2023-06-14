e024ca54e9f01d34469d698f9a80a1b5357b1d13
dwidhanis <dwidhanis@gmail.com>
2023-06-14 22:08:48 +0700

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color yellowColor = const Color(0xfffaff00);
+Color blackColor = const Color(0xff000000);
+Color whiteColor = const Color(0xffffffff);
+Color greenColor = const Color(0xff33ff00);
+Color greyColor = const Color(0xffd9d9d9);
+Color blueColor = const Color(0xff00a6fb);
+
+double edge = 20;
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  color: whiteColor,
+);
+
+TextStyle whitelightTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle whitemedTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyColor,
+);
+
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: blueColor,
+);
+
