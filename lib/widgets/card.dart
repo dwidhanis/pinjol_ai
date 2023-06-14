3e636e5539fa7353bd2e6e025dc69b65ae795128
dwidhanis <dwidhanis@gmail.com>
2023-06-14 22:10:07 +0700

    add widget card

---
+++ lib/widgets/card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pinjol_ai/theme.dart';
+
+class Card extends StatelessWidget {
+  const Card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 240,
+      color: whiteColor,
+    );
+  }
+}
